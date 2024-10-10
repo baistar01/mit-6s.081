6a665fbbfefc4b602f9b6523585033050c75fac2
baistar01 <lucky@DESKTOP-68EVKKD>
2024-10-10 16:16:38 +0800

    第二章

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,15 +6,16 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
 {
   int n;
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   exit(n);
-  return 0;  // not reached
+  return 0; // not reached
 }
 
 uint64
@@ -33,7 +34,7 @@ uint64
 sys_wait(void)
 {
   uint64 p;
-  if(argaddr(0, &p) < 0)
+  if (argaddr(0, &p) < 0)
     return -1;
   return wait(p);
 }
@@ -44,10 +45,10 @@ sys_sbrk(void)
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
@@ -58,12 +59,14 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n)
+  {
+    if (myproc()->killed)
+    {
       release(&tickslock);
       return -1;
     }
@@ -78,7 +81,7 @@ sys_kill(void)
 {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
@@ -94,4 +97,28 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64
+sys_trace(void)
+{
+  int n;
+  argint(0, &n);           // 接收参数
+  myproc()->tracemask = n; // 赋给mask
+  return 0;
+}
+
+uint64 get_used_proc(void);
+uint64 get_free_memory(void);
+
+uint64 sys_sysinfo()
+{
+  uint64 st;
+  argaddr(0, &st);  // 获取从用户空间传入的指针。
+  struct sysinfo p; // 将信息存在结构体中
+  p.nproc = get_used_proc();
+  p.freemem = get_free_memory();
+  if (copyout(myproc()->pagetable, st, (char *)&p, sizeof(p)) < 0) // 拷贝回用户空间
+    return -1;
+  return 0;
 }
