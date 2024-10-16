4c793139eea266af86e23a0d7e9c3dfdc3661225
baistar01 <lucky@DESKTOP-68EVKKD>
2024-10-16 17:01:04 +0800

    第五章cow完结

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -14,28 +14,83 @@ void freerange(void *pa_start, void *pa_end);
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
-struct run {
+struct run
+{
   struct run *next;
 };
 
-struct {
+// ok
+struct
+{
   struct spinlock lock;
   struct run *freelist;
+  char *ref_page; // 声明一个指针
+  int page_cnt;
+  char *end_;
 } kmem;
 
-void
-kinit()
-{
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
-}
-
-void
-freerange(void *pa_start, void *pa_end)
+int pagecnt(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  int cnt = 0;
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
+  {
+    cnt++;
+  }
+  return cnt;
+}
+
+// ok
+void kinit()
+{
+  initlock(&kmem.lock, "kmem");
+  kmem.page_cnt = pagecnt(end, (void *)PHYSTOP);
+  printf("page_cnt: %d\n", kmem.page_cnt);
+
+  kmem.ref_page = end;
+  for (int i = 0; i < kmem.page_cnt; ++i)
+  {
+    kmem.ref_page[i] = 0;
+  }
+  kmem.end_ = kmem.ref_page + kmem.page_cnt;
+
+  freerange(kmem.end_, (void *)PHYSTOP);
+}
+
+// ok
+int page_index(uint64 pa)
+{
+  pa = PGROUNDDOWN(pa);
+  int res = (pa - (uint64)kmem.end_) / PGSIZE;
+  if (res < 0 || res >= kmem.page_cnt)
+  {
+    panic("page_index illegal");
+  }
+  return res;
+}
+
+void incr(void *pa)
+{
+  int index = page_index((uint64)pa);
+  acquire(&kmem.lock);
+  kmem.ref_page[index]++;
+  release(&kmem.lock);
+}
+
+void desc(void *pa)
+{
+  int index = page_index((uint64)pa);
+  acquire(&kmem.lock);
+  kmem.ref_page[index]--;
+  release(&kmem.lock);
+}
+
+void freerange(void *pa_start, void *pa_end)
+{
+  char *p;
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
     kfree(p);
 }
 
@@ -43,23 +98,48 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void *pa)
 {
+  int index = page_index((uint64)pa);
+  if (kmem.ref_page[index] > 1)
+  {
+    desc(pa);
+    return;
+  }
+  if (kmem.ref_page[index] == 1)
+  {
+    desc(pa);
+  }
   struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+  r = (struct run *)pa;
 
   acquire(&kmem.lock);
   r->next = kmem.freelist;
   kmem.freelist = r;
   release(&kmem.lock);
+}
+
+void print_free_pages_cnt()
+{
+  struct run *r;
+  int cnt = 0;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  while (r->next)
+  {
+    r = r->next;
+    cnt++;
+  }
+  release(&kmem.lock);
+  printf("print_free_pages_cnt: %d\n", cnt);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -72,11 +152,15 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if (r)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  if (r)
+  {
+    memset((char *)r, 5, PGSIZE); // fill with junk
+    // print_free_pages_cnt();
+    incr(r);
+  }
+  return (void *)r;
 }
